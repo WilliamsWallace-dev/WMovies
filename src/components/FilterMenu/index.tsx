83f98ff3229e82c1f8b1121103e083766dc53d1e
Williams Wallace <wwallace.dev@gmail.com>
2023-09-22 15:17:43 -0300

    FilterMenu has been modified

--- src/components/FilterMenu/index.tsx
+++ src/components/FilterMenu/index.tsx
@@ -9,17 +9,17 @@ import {ReverCardsUtil, filterSelectUtil} from "../../utils/filterUtil"
 import { useContext, useEffect } from "react"
 import { AppContext } from "../../context/AppContext"
 
-const handleFieldsChange = (e : {currentTarget : {value : string}}, setSearch : (value: React.SetStateAction<string>) => void,setCards: (value: React.SetStateAction<CardType[]>) => void, search : string, typeContent: string, moviesList: CardType[], seriesList: CardType[], animesList: CardType[])=>{
+const handleFieldsChange = (e : {currentTarget : {value : string}}, setSearch : (value: React.SetStateAction<string>) => void,setCards: (value: React.SetStateAction<CardType[]>) => void, typeContent: string, moviesList: CardType[], seriesList: CardType[], animesList: CardType[])=>{
     setSearch(e.currentTarget.value)
-    SearchCards(e,setCards,search,setSearch,typeContent, moviesList, seriesList, animesList)
+    SearchCards(e,setCards,setSearch,typeContent, moviesList, seriesList, animesList)
 }
 
 const changeLabel = (parentNode : string, search : string)=>{
     changeLabelUtil(parentNode, search)
 }
 
-const SearchCards = async (e : {currentTarget : {value : string}} , setCards: (value: React.SetStateAction<CardType[]>) => void, search : string, setSearch: (value: React.SetStateAction<string>) => void, typeContent: string, moviesList: CardType[], seriesList: CardType[], animesList: CardType[])=>{
-    SearchCardsUtil(e,setCards,search,setSearch,typeContent, moviesList, seriesList, animesList)
+const SearchCards = async (e : {currentTarget : {value : string}} , setCards: (value: React.SetStateAction<CardType[]>) => void, setSearch: (value: React.SetStateAction<string>) => void, typeContent: string, moviesList: CardType[], seriesList: CardType[], animesList: CardType[])=>{
+    SearchCardsUtil(e,setCards,setSearch,typeContent, moviesList, seriesList, animesList)
 }
 
 
@@ -41,7 +41,7 @@ export const FilterMenu = ({parentNode,cards,setCards, search, setSearch,typeCon
                 <section className="filterMenu container flex-space-around w-100" >
                             <h3 className="title mr-3">{typeContent}</h3>
                             <div className="inputSearch">
-                                <input className="inputSearchTarget" type="text" id="search" name="search" value={search} onChange={(e)=>{handleFieldsChange(e,setSearch,setCards,search,typeContent, moviesList, seriesList, animesList)}}  onBlur={()=>{changeLabel(parentNode,search)}} placeholder=""/>
+                                <input className="inputSearchTarget" type="text" id="search" name="search" value={search} onChange={(e)=>{handleFieldsChange(e,setSearch,setCards,typeContent, moviesList, seriesList, animesList)}}  onBlur={()=>{changeLabel(parentNode,search)}} placeholder=""/>
                                 <label id="searchLabel" htmlFor="search">Digite o Título do Filmes, Serie...</label>
                             </div>  
                             <div className="selectFilter-menu">
@@ -76,7 +76,7 @@ export const FilterMenu = ({parentNode,cards,setCards, search, setSearch,typeCon
             <section className="filterMenu Search container" >
                             {/* <h3 className="title mr-5">Pesquisa</h3> */}
                             <div className="inputSearch">
-                                <input className="inputSearchTarget" type="text" id="search" value={search} onChange={(e)=>{handleFieldsChange(e,setSearch,setCards,search,typeContent, moviesList, seriesList, animesList)}}  onBlur={()=>{changeLabel(parentNode,search)}} placeholder=""/>
+                                <input className="inputSearchTarget" type="text" id="search" value={search} onChange={(e)=>{handleFieldsChange(e,setSearch,setCards,typeContent, moviesList, seriesList, animesList)}}  onBlur={()=>{changeLabel(parentNode,search)}} placeholder=""/>
                                 <label id="searchLabel" htmlFor="search">Digite o Título do Filmes, Serie...</label>
                             </div>  
             </section>    
--- src/utils/searchUtil/index.ts
+++ src/utils/searchUtil/index.ts
@@ -17,7 +17,7 @@ export const changeLabelUtil = (parentNode : string, search : string)=>{
         }  
 }
 
-export const SearchCardsUtil = async (e : {currentTarget : {value : string}}, setCards: (value: React.SetStateAction<CardType[]>) => void, search : string, setSearch: (value: React.SetStateAction<string>) => void,typeContent: string, moviesList: CardType[], seriesList: CardType[], animesList: CardType[])=>{
+export const SearchCardsUtil = async (e : {currentTarget : {value : string}}, setCards: (value: React.SetStateAction<CardType[]>) => void, setSearch: (value: React.SetStateAction<string>) => void,typeContent: string, moviesList: CardType[], seriesList: CardType[], animesList: CardType[])=>{
     // console.log(search.length,e.keyCode)
     // console.log("To aqui ")
     // alert("to aqui")
